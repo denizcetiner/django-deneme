9b545bc85d328a7b01eb7f4b3564420ed58b6ad7
denizcetiner <deniz.cetiner94@gmail.com>
2017-04-13 16:57:05 +0000

    Changed project theme from "polls" to "blog" because of reason.

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
